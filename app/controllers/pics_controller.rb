22dcddde2c58748c8e5ea392ce985147361af833
Kevin <kyrodriguez97@gmail.com>
2018-06-02 18:58:14 -0500

    Create post

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,5 +1,23 @@
 class PicsController < ApplicationController
   def index
+  end
 
+  def new
+    @pic = Pic.new
+  end
+
+  def create
+    @pic = Pic.new(pic_params)
+    if @pic.save
+      redirect_to @pic, notice: "It was posted!"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def pic_params
+    params.require(:pic).permit(:title, :description)
   end
 end
